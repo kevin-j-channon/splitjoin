aca6d56846d10de0093b5a618d80bdab4c856cbf
kevin <kevinchannon@gmail.com>
2021-10-27 19:44:15 +0100

    Adding test for joining wide strings

--- splitjoin/splitjoin.cpp
+++ splitjoin/splitjoin.cpp
@@ -29,5 +29,21 @@ namespace splitjoin
             // THEN
             Assert::AreEqual( std::string( "One small step for man..."), sentence );
 		}
+
+        TEST_METHOD( join_wide_strings_with_spaces )
+        {
+            // Given: a vector of wide strings
+            // When: I join with a space
+            // Then: The result is a single wide string with the items separated by space characters.
+
+            // GIVEN
+            const auto items = std::vector<std::wstring>{ L"One", L"small", L"step", L"for", L"man..." };
+
+            // WHEN
+            const auto sentence = join( items, L' ' );
+
+            // THEN
+            Assert::AreEqual( std::wstring( L"One small step for man..." ), sentence );
+        }
 	};
 }
