3e26ae4b48a3cfc1abc0ede4d475d6bbdc469b27
kevin <kevinchannon@gmail.com>
2021-10-27 19:11:54 +0100

    Add project files.

---
+++ splitjoin/join.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+template<typename Range_T>
+Range_T join( const Range_T& range, typename Range_T::value_type delimiter )
+{
+    return Range_T();
+}
---
+++ splitjoin/splitjoin.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace splitjoin
+{
+	TEST_CLASS(splitjoin)
+	{
+	public:
+		
+		TEST_METHOD(join_strings_with_spaces)
+		{
+		}
+	};
+}
